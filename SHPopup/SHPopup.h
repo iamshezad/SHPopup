5d201b67519c5dd65db32a8ea976a4ccc85ade04
Focaloid Technologies <shezad@focaloid.com>
2017-12-14 14:23:59 +0530

    Initial Commit

---
+++ SHPopup/SHPopup.h
@@ -0,0 +1,19 @@
+//
+//  SHPopup.h
+//  SHPopup
+//
+//  Created by Focaloid Technologies on 14/12/17.
+//  Copyright © 2017 Focaloid Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SHPopup.
+FOUNDATION_EXPORT double SHPopupVersionNumber;
+
+//! Project version string for SHPopup.
+FOUNDATION_EXPORT const unsigned char SHPopupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SHPopup/PublicHeader.h>
+
+
